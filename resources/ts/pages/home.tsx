1149c98569cb2f4e5194686f0936960d97f766e6
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-12-12 22:04:15 -0600

    update homepage

--- resources/ts/pages/home.tsx
+++ resources/ts/pages/home.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-slate-800 to-slate-700">
-      <h1 className="text-white">Laravel + Inertia + React + Typescript</h1>
+      <h1 className="text-3xl lg:text-4xl text-white">
+        Laravel + Inertia + React + Typescript template
+      </h1>
+      <p className="text-white mt-4">
+        This is a template for building Laravel applications with Inertia.js,
+        React, and Typescript.
+      </p>
     </div>
   );
 }
