2bd7c19d2c4b17bb551aedd5c88dbb47f2d8c3a1
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-12-12 21:57:08 -0600

    inertia + react + ts integration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  resolve: {
+    alias: {
+      "@": "/resources/ts",
+    },
+  },
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/ts/app.tsx"],
+      refresh: true,
+      resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+        return pages[`./pages/${name}.tsx`];
+      },
+    }),
+    react(),
+  ],
 });
